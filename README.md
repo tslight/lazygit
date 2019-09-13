14e6c32081e3d26c5bcf0996a85b090ad42142ea
tslight <tslight@pm.com>
2019-09-13 01:58:15 +0100

    add default dest, config file dest and make dir if not there

--- README.md
+++ README.md
@@ -4,17 +4,25 @@ So far this only supports Gitlab, but hopefully it will support GitHub too
 soon...
 
 Firstly you need to generate a Gitlab Personal Access Token from
-[here](https://gitlab.com/profile/personal_access_tokens).  To authenicate to
-Gitlab copy `token.example.json` to `token.json` and add your Gitlab Personal
-Access Token
+[here](https://gitlab.com/profile/personal_access_tokens).
+
+To authenicate to Gitlab copy `config.example.json` to `config.json` and add your
+Gitlab Personal Access Token.
+
+To specify which directory you'd like to clone/update change the `destination`
+value in `config.json`.
 
 ```javascript
 {
+  "dest": "/path/to/location/for/repos"
   "token" : "GITLAB_TOKEN"
 }
-
 ```
 
-Then run `npm install` in the project root, to download dependancies.
+Then run `npm install` in the project root, to download dependancies, and `npm
+link` to install as CLI app called `lazygitlab`
 
-Finally run `npm run gitlab /path/to/directory` and you will have all the repos :smiley.
+If you want to overwrite the default destination of `~/src` or the one you
+specified in `config.json`, simply append it to the command like so:
+
+`lazygitlab /path/to/some/other/repo/location`
--- lazygitlab.js
+++ lazygitlab.js
@@ -1,25 +1,42 @@
 #!/usr/bin/env node
 'use strict';
 const Promise = require('bluebird');
-const rp = require('request-promise');
-const lodash = require('lodash');
-const cmd = require('node-cmd');
-const fs = require('fs');
-const tokenJson = require('./token');
-const token = tokenJson.token;
-const destination = process.argv[2];
-const group = process.argv[3];
-const host = 'https://www.gitlab.com';
+const rp      = require('request-promise');
+const lodash  = require('lodash');
+const cmd     = require('node-cmd');
+const fs      = require('fs');
+const homedir = require('os').homedir();
 
-if (destination == undefined) {
-  console.log(`No directory argument... Aborting!\n`);
-  process.exit();
-} else if (fs.existsSync(destination) == false) {
-  console.log(`${destination} does not exist... Aborting!\n`);
-  process.exit();
-} else {
-  console.log(`Cloning or updating repos to ${destination}...`);
+const nc  = "\x1b[0m"
+const br  = "\x1b[1m"
+const red = "\x1b[1m\x1b[31m"
+const grn = "\x1b[1m\x1b[32m"
+const yel = "\x1b[1m\x1b[33m"
+const blu = "\x1b[1m\x1b[34m"
+const mag = "\x1b[1m\x1b[35m"
+const cyn = "\x1b[1m\x1b[36m"
+const wht = "\x1b[1m\x1b[37m"
+
+const config = require('./config');
+const token  = config.token;
+
+if (process.argv[2] == undefined) {
+  if (config.dest == undefined) {
+    var dest = `${homedir}/src`;
+  } else {
+    var dest = config.dest;
+  }
 }
+
+const host  = 'https://www.gitlab.com';
+const group = process.argv[3];
+
+//https://stackoverflow.com/a/40686853
+if (fs.existsSync(dest) == false) {
+  fs.mkdirSync(dest, { recursive: true });
+}
+
+console.log(`Cloning or updating ${host} repos to ${dest}...`);
 
 rp.get('https://www.gitlab.com/api/v4/groups\?per_page\=999', {
   json: true,
@@ -73,15 +90,15 @@ rp.get('https://www.gitlab.com/api/v4/groups\?per_page\=999', {
   Promise.all(promises).then(() => {
     // console.log(pgits);
     for (let git of pgits) {
-      let path = destination + '/' + git.substring(git.indexOf(':') + 1);
+      let path = dest + '/' + git.substring(git.indexOf(':') + 1);
       path = path.replace('.git', '');
       path = path.replace('//', '/');
       if (fs.existsSync(path)) {
 	cmd.run(`git -C ${path} pull`);
-	console.log(`UPDATED ${path} :-)`);
+	console.log(`${grn}UPDATED${nc} ${path} :-)`);
       } else {
 	cmd.run(`git clone ${git} ${path}`);
-	console.log(`CLONED ${git} to ${path} :-)`);
+	console.log(`${yel}CLONED${nc} ${git} to ${path} :-)`);
       }
     }
   });
