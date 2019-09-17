da86aa066fb01c9057c0e7474aa138e49e2f54f8
tslight <tslight@pm.com>
2019-09-17 17:26:56 +0100

    attempting to modularise the bugger

--- lazygitlab-axios.js
+++ lazygitlab-axios.js
@@ -18,26 +18,35 @@ gl = axios.create({
 
 let gids = [];
 let projects = [];
-return gl.get('/api/v4/groups\?per_page\=999')
-  .then((groups) => {
-    return groups.data.map((g) => {
-      return gids.push(g.id);
-    });
-  })
-  .then(() => {
-    return gids.map((id) => {
-      return gl.get(`/api/v4/groups/${id}/projects\?per_page\=999`)
-	.then((repos) => {
-	  return repos.data.map((r) => {
-	    console.log(r.ssh_url_to_repo)
-	    return projects.push(r.ssh_url_to_repo)
-	  })
-	})
-	.catch((err) => {
-	  throw err;
+
+getUrls = () => {
+  return gids.map((id) => {
+    return gl.get(`/api/v4/groups/${id}/projects\?per_page\=999`)
+      .then((repos) => {
+	// console.log(repos);
+	return repos.data.map((r) => {
+	  return projects.push(r.ssh_url_to_repo);
 	});
-    })
+      })
+      .catch((err) => {
+	throw err;
+      });
+  });
+};
+
+getGroups = () => {
+  return gl.get('/api/v4/groups\?per_page\=999')
+    .then((groups) => {
+      return groups.data.map((g) => {
+	return gids.push(g.id);
+      });
+    });
+};
+
+return getGroups()
+  .then((groups) => {
+    return getUrls(groups);
   })
-  .then(() => {
-    console.log(projects);
-  })
+  .then((urls) => {
+    console.log(urls);
+  });
