6b19cb0a3fd25b9e503651a12c0b698903b3c06f
tslight <tslight@pm.com>
2019-09-23 15:43:56 +0100

    getting there...

--- lazygitlab-axios.js
+++ lazygitlab-axios.js
@@ -1,52 +1,35 @@
 axios = require('axios');
-// gitlab = axios.create({
-//   baseURL: "https://www.gitlab.com",
-//   headers: {
-//     'PRIVATE-TOKEN': "jvBukuh6y8xTd6MyLwFz"
-//   }
-// });
-// console.log(gitlab);
-// groups = gitlab.get('/api/v4/groups\?per_page\=999');
-// console.log(groups);
 
 gl = axios.create({
   baseURL: "https://www.gitlab.com",
-  json: true,
-  qs: {simple: true},
   headers: {'PRIVATE-TOKEN': "jvBukuh6y8xTd6MyLwFz"}
 });
 
-let gids = [];
-let projects = [];
-
-getUrls = () => {
-  return gids.map((id) => {
-    return gl.get(`/api/v4/groups/${id}/projects\?per_page\=999`)
-      .then((repos) => {
-	// console.log(repos);
-	return repos.data.map((r) => {
-	  return projects.push(r.ssh_url_to_repo);
-	});
-      })
-      .catch((err) => {
-	throw err;
+getUrls = (id) => {
+  return gl.get(`/api/v4/groups/${id}/projects\?per_page\=999`)
+    .then((repos) => {
+      return repos.data.map((r) => {
+	return r.ssh_url_to_repo;
       });
-  });
+    });
 };
 
 getGroups = () => {
   return gl.get('/api/v4/groups\?per_page\=999')
     .then((groups) => {
       return groups.data.map((g) => {
-	return gids.push(g.id);
+	return g.id;
       });
     });
 };
 
 return getGroups()
-  .then((groups) => {
-    return getUrls(groups);
-  })
-  .then((urls) => {
-    console.log(urls);
+  .then((gids) => {
+    // console.log(gids);
+    return gids.map((id) => {
+      return getUrls(id)
+	.then((urls) => {
+	  // console.log(urls);
+	});
+    });
   });
