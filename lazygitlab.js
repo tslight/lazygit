8cb084010b005f50b61f1fd5f403e05b70a5f3de
tslight <tslight@pm.com>
2019-09-24 23:47:29 +0100

    add some comments - there's no way I'll remember this!

--- lazygitlab.js
+++ lazygitlab.js
@@ -4,12 +4,15 @@ const gitlab = require('./lib/gitlab');
 
 return gitlab.getGroups()
   .then((gids) => {
+    // https://stackoverflow.com/a/39452119 - Using a function that returns
+    // promises within a map returns an array of promises. Duh...
     let groupUrls = gids.map((id) => {
       return gitlab.getUrls(id);
     });
-    Promise.all(groupUrls)
+    // ...So we have to resolve all these promises before proceeding.
+    Promise.all(groupUrls) // Returns array of arrays...
       .then((groupUrls) => {
-	return groupUrls.flat();
+	return groupUrls.flat(); // ...Which we need to flatten.
       })
       .then((allUrls) => {
 	return allUrls.map((url) => {
