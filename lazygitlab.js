3a0a01090d5c59382f977296dcac42389194dbe2
tslight <tslight@pm.com>
2019-09-24 12:21:58 +0100

    api stuff in seperate library

--- lazygitlab.js
+++ lazygitlab.js
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
-const axios     = require('axios');
-const fs        = require('fs');
-const git       = require('./lib/git');
-const args      = require('./lib/args');
-const token     = args.chkToken();
-const dest      = args.chkDest();
-const host      = 'https://www.gitlab.com';
-const gitlabApi = axios.create({
-  baseURL: host,
-  headers: {'PRIVATE-TOKEN': token}
-});
+const fs     = require('fs');
+const git    = require('./lib/git');
+const gitlab = require('./lib/gitlab');
+const args   = require('./lib/args');
+const dest   = args.chkDest();
+const host   = 'https://www.gitlab.com';
 
 runGit = (url) => {
   let path = dest + '/' + url.substring(url.indexOf(':') + 1);
@@ -21,36 +16,11 @@ runGit = (url) => {
   (fs.existsSync(path) ? git.pull(path) : git.clone(url, path));
 };
 
-getUrls = (id) => {
-  let slug = `/api/v4/groups/${id}/projects\?per_page\=999`;
-  return gitlabApi.get(slug)
-    .then((repos) => {
-      return repos.data.map((r) => {
-	return r.ssh_url_to_repo;
-      });
-    });
-};
-
-getGroups = () => {
-  let slug = `/api/v4/groups\?per_page\=999`;
-  return gitlabApi.get(slug)
-    .then((groups) => {
-      return groups.data.filter((group) => {
-	return group.full_path.startsWith(args.group);
-      });
-    })
-    .then((data) => {
-      return data.map((group) => {
-	return group.id;
-      });
-    });
-};
-
 lazygit = () => {
-  return getGroups()
+  return gitlab.getGroups()
     .then((gids) => {
       return gids.map((id) => {
-	return getUrls(id)
+	return gitlab.getUrls(id)
 	  .then((urls) => {
 	    return urls.map((url) => {
 	      return runGit(url);
