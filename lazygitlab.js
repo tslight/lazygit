3aa46e0e54d0a3671331ac95dac019bca206ea38
tslight <tslight@pm.com>
2019-09-24 20:44:23 +0100

    terser output

--- lazygitlab.js
+++ lazygitlab.js
@@ -1,51 +1,27 @@
 #!/usr/bin/env node
-const fs     = require('fs');
 const git    = require('./lib/git');
 const gitlab = require('./lib/gitlab');
-const args   = require('./lib/args');
-const dest   = args.chkDest();
-const host   = 'https://www.gitlab.com';
 
-runGit = (url) => {
-  let path = dest + '/' + url.substring(url.indexOf(':') + 1);
-  path = path.replace('.git', '').replace('//', '/');
-  if (args.status) {
-    git.status(path);
-    return;
-  }
-  if (args.fetch) {
-    (fs.existsSync(path) ? git.fetch(path) : git.clone(url, path));
-  } else {
-    (fs.existsSync(path) ? git.pull(path) : git.clone(url, path));
-  }
-};
-
-// lazygit = () => {
-//   return gitlab.getGroups()
-//     .then((gids) => {
-//       return gids.map((id) => {
-//	return gitlab.getUrls(id);
-//       });
-//     })
-//     .then((urls) => {
-//       return urls.map((url) => {
-//	return runGit(url);
-//       });
+// return gitlab.getGroups()
+//   .then((gids) => {
+//     return gids.map((id) => {
+//       return gitlab.getUrls(id);
 //     });
-// };
+//   })
+//   .then((urls) => {
+//     return urls.map((url) => {
+//       return runGit(url);
+//     });
+//   });
 
-lazygit = () => {
-  return gitlab.getGroups()
-    .then((gids) => {
-      return gids.map((id) => {
-	return gitlab.getUrls(id)
-	  .then((urls) => {
-	    return urls.map((url) => {
-	      return runGit(url);
-	    });
+return gitlab.getGroups()
+  .then((gids) => {
+    return gids.map((id) => {
+      return gitlab.getUrls(id)
+	.then((urls) => {
+	  return urls.map((url) => {
+	    return git.run(url);
 	  });
-      });
+	});
     });
-};
-
-return lazygit();
+  });
